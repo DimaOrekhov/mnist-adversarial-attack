efe2e335efb59dc4d3300249c44f4563e7cf4dbc
Dmitrii Orekhov <dmitrii.orekhov@jetbrains.com>
2020-10-18 05:59:03 +0300

    Added early stopping

--- save_all_batch_metrics_callback.py
+++ save_all_batch_metrics_callback.py
@@ -1,18 +1,18 @@
 from catalyst.core import Callback, CallbackOrder
-from typing import List, Union, Optional
+from typing import List, Union, Optional, Dict
 
 
 class SaveAllBatchMetricsCallback(Callback):
 
-    def __init__(self, logging_list: List, metric_keys: Optional[Union[str, List[str]]] = None):
+    def __init__(self, logging_dict: Dict, metric_keys: Optional[Union[str, List[str]]] = None):
         super().__init__(order=CallbackOrder.Logging)
-        self.logging_list = logging_list
+        self.logging_dict = logging_dict
         self.metric_keys = metric_keys
 
     def on_batch_end(self, runner: "IRunner"):
         if self.metric_keys is None:
             # log everything
-            return self.logging_list.append(runner.batch_metrics)
-        self.logging_list.append(
+            return self.logging_dict[runner.loader_name].append(runner.batch_metrics)
+        self.logging_dict[runner.loader_name].append(
             {key: runner.batch_metrics[key] for key in self.metric_keys}
         )
--- utils.py
+++ utils.py
@@ -1,7 +1,36 @@
 from sklearn.base import BaseEstimator, ClassifierMixin
 from sklearn.metrics import accuracy_score
 import torch
+from torch.utils.data import Dataset
 import numpy as np
+from typing import Sequence, Union, Optional
+
+
+class DatasetWithTransforms(Dataset):
+
+    def __init__(
+        self,
+        dataset,
+        transforms,
+        idx_to_apply: Optional[Union[int, Sequence[int]]]=None
+    ):
+        self.dataset = dataset
+        self.transforms = transforms
+        self.idx_to_apply = (idx_to_apply if isinstance(idx_to_apply, Sequence) 
+                                          else [idx_to_apply])
+
+    def __len__(self):
+        return len(self.dataset)
+
+    def __getitem__(self, idx):
+        element = self.dataset[idx]
+        if self.idx_to_apply is None:
+            return self.transforms(element)
+        if isinstance(element, tuple):
+            element = list(element)
+        for i in self.idx_to_apply:
+            element[i] = self.transforms(element[i])
+        return element
 
 
 class TorchToSklearnClassifierWrapper(BaseEstimator, ClassifierMixin):
@@ -15,4 +44,3 @@ class TorchToSklearnClassifierWrapper(BaseEstimator, ClassifierMixin):
 
     def predict(self, x):
         return np.argmax(self.predict_proba(x), axis=-1)
-
